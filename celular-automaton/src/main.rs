b2f863d5b6e52fccb7d11742a46978258993cf3a
rennelou <rennelou@outlook.com>
2020-10-28 01:35:54 -0300

    glider que vira block

--- celular-automaton/src/main.rs
+++ celular-automaton/src/main.rs
@@ -3,13 +3,13 @@ use ndarray::{arr2};
 const SIZE: usize = 3;
 
 fn main() {
-    let mut states = arr2(&[[0, 1, 1],
-                            [1, 0, 1],
+    let mut states = arr2(&[[1, 1, 1],
+                            [1, 0, 0],
                             [0, 1, 0]]);
 
     println!("t0\n{}", states);
-    for tick in 1..4 {
-        states = automaton::tick(automaton::circular_boundary, SIZE, &states);
+    for tick in 1..6 {
+        states = automaton::tick(automaton::zero_boundary, SIZE, &states);
         println!("t{}\n{}", tick, states);
     }
 }
