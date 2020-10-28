a9432b1086287f8e89c552b8c1e3a3ad42a9b919
rennelou <rennelou@outlook.com>
2020-10-28 00:32:13 -0300

    limite circular

--- celular-automaton/src/lib.rs
+++ celular-automaton/src/lib.rs
@@ -1,19 +1,14 @@
-use ndarray::{Array2, s};
+use ndarray::{Array2};
 
-const SIZE: usize = 3;
-
-pub fn tick(states: &Array2<u8>) -> Array2<u8>{
+pub fn tick(get_neighborhood: fn (&Array2<u8>, usize, (usize, usize)) -> Array2<u8>, size: usize, states: &Array2<u8>) -> Array2<u8>{
     
-    let mut new_states = Array2::<u8>::zeros((SIZE, SIZE));
+    let mut new_states = Array2::<u8>::zeros((size, size));
     
     for (i, row) in states.outer_iter().enumerate() {
         for (j, _) in row.iter().enumerate() {
-            let (i_start, i_end) = validate_range(i);
-            let (j_start, j_end) = validate_range(j);
-
-            let subarray = states.slice(s![i_start..i_end, j_start..j_end]);
             let value = states[[i, j]];
-            new_states[[i, j]] = transition(&subarray.to_owned(), value);
+            let neighborhood = get_neighborhood(&states, size, (i, j));
+            new_states[[i, j]] = transition(&neighborhood, value);
         }
     }
 
@@ -27,51 +22,82 @@ fn transition(mat: &Array2<u8>, value: u8) -> u8 {
     }
 
     if counter >= 3 {
-        return 1
+        return 1;
     } else {
-        return 0
+        return 0;
     }
 }
 
-fn validate_range(index : usize) -> (usize, usize) {
-    const LIMIT_END: usize = SIZE - 1;
+pub fn zero_neighborhood(mat: &Array2<u8>, rad: usize, point: (usize, usize)) -> Array2<u8> {
+    let mut neighborhood = Array2::<u8>::zeros((rad, rad));
+    let mask = [-1, 0, 1];
+    
+    for (i, i_val) in mask.iter().enumerate() {
+        
+        let i_tranformed = (point.0 as i32) + *i_val;
+        for (j, j_val) in mask.iter().enumerate() {
+            
+            let j_tranformed = (point.1 as i32) + *j_val;
 
-    let start = match index{
-        0 => 0,
-        other => other - 1,
-    };
-
-    let end = match index {
-        LIMIT_END => SIZE,
-        other => other + 2,
-    };
-
-    (start, end)
+            if i_tranformed < 0 || i_tranformed >= rad as i32 || j_tranformed < 0 || j_tranformed >= rad as i32 {
+                neighborhood[[i, j]] = 0;
+            } else {
+                neighborhood[[i, j]] = mat[[i_tranformed as usize, j_tranformed as usize]];
+            }
+        }    
+    }
+    
+    return neighborhood.to_owned();
 } 
+
+pub fn circular_boundary(mat: &Array2<u8>, rad: usize, point: (usize, usize)) -> Array2<u8> {
+    let mut neighborhood = Array2::<u8>::zeros((rad, rad));
+    let mask = [-1, 0, 1];
+
+    let tranform = |p: usize, val: i32| { return (((p as i32) + (rad as i32) + val) % (rad as i32)) as usize; };
+
+    for (i, i_val) in mask.iter().enumerate() {
+        
+        let i_circle = tranform(point.0, *i_val);
+        for (j, j_val) in mask.iter().enumerate() {
+            
+            let j_circle = tranform(point.1, *j_val);
+            neighborhood[[i, j]] = mat[[i_circle, j_circle]]
+        }    
+    }
+    
+    return neighborhood.to_owned();
+}
 
 //TESTS
 #[cfg(test)]
 mod tests {
     use super::*;
     use ndarray::arr2;
+    const SIZE: usize = 3;
 
     #[test]
     fn validate_range_test() {
-        //size = 3
-        let i = 0;
-        let j = 0;
-        assert_eq!((0, 2), validate_range(i));
-        assert_eq!((0, 2), validate_range(j));
+        let mat = arr2(&[[1, 1, 1],
+                         [1, 0, 1],
+                         [1, 1, 1]]);
 
-        let i = 1;
-        let j = 1;
-        assert_eq!((0, 3), validate_range(i));
-        assert_eq!((0, 3), validate_range(j));
+        let result = arr2(&[[0, 0, 0],
+                            [0, 1, 1],
+                            [0, 1, 0]]);
+        assert_eq!(result, zero_neighborhood(&mat, SIZE, (0, 0)));
+        
+        let result = arr2(&[[0, 1, 0],
+                            [1, 1, 0],
+                            [0, 0, 0]]);
+        assert_eq!(result, zero_neighborhood(&mat, SIZE, (2, 2)));
 
-        let i = 1;
-        let j = 2;
-        assert_eq!((0, 3), validate_range(i));
-        assert_eq!((1, 3), validate_range(j));
+        let result = arr2(&[[1, 1, 0],
+                            [0, 1, 0],
+                            [1, 1, 0]]);
+        assert_eq!(result, zero_neighborhood(&mat, SIZE, (1, 2)));
+
+        assert_eq!(mat, zero_neighborhood(&mat, SIZE, (1, 1)));
     }
 
     #[test]
@@ -95,6 +121,6 @@ mod tests {
                               [1, 1, 1],
                               [0, 1, 0]]);
 
-        assert_eq!(expected, tick(&mat));
+        assert_eq!(expected, tick(zero_neighborhood, SIZE, &mat));
     }
 }
--- celular-automaton/src/main.rs
+++ celular-automaton/src/main.rs
@@ -1,5 +1,7 @@
 use ndarray::{arr2};
 
+const SIZE: usize = 3;
+
 fn main() {
     let mut states = arr2(&[[0, 1, 1],
                             [1, 0, 1],
@@ -7,7 +9,7 @@ fn main() {
 
     println!("t0\n{}", states);
     for tick in 1..4 {
-        states = automaton::tick(&states);
+        states = automaton::tick(automaton::circular_boundary, SIZE, &states);
         println!("t{}\n{}", tick, states);
     }
 }
